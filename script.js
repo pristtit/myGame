1c0311338317892ff098e79c9bc418c33ff59f14
pristtit <pristtit@mail.ru>
2022-01-26 16:27:37 +0300

    гтовый проект; подкоректировал незначительно

--- script.js
+++ script.js
@@ -2,19 +2,14 @@ let canvas = document.getElementById('game');
 let context = canvas.getContext('2d');
 context.font = "50px Arial";
 
-let saves;
-!localStorage.saves ? saves = [] : saves = JSON.parse(localStorage.getItem('saves'));
-
-const updateLocal = () => {
-	localStorage.setItem('saves', JSON.stringify(saves));
-}
-
 const backgroundImg = new Image();
 backgroundImg.src = "img/fon.jpg";
 
 const barrel = new Image();
 barrel.src = "img/barrel.png";
 
+const startSpeed = 10;
+const endSpeed = 3;
 let y = 200;
 let x = getRandomInt(200);
 let rnd = getRandomInt(9);
@@ -22,37 +17,29 @@ let step = 0;
 let point = 0;
 let age = 0;
 let maxAge = 10;
-const startSpeed = 10;
-const endSpeed = 3;
+let result = false;
+let saves;
 let best;
-let result;
+
+!localStorage.saves ? saves = [] : saves = JSON.parse(localStorage.getItem('saves'));
 !localStorage.saves ? best = 0 : best = Math.max(...JSON.parse(localStorage.getItem('saves')));
 
-function startpack() {
-	let y = 200;
-	let x = getRandomInt(200);
-	let rnd = getRandomInt(9);
-	let step = 0;
-	let point = 0;
-	let age = 0;
-	let maxAge = 10;
-	const startSpeed = 10;
-	const endSpeed = 3;
-	let best;
-	let result;
+const updateLocal = () => {
+	localStorage.setItem('saves', JSON.stringify(saves));
 }
-backgroundImg.onload = function () {
-	game();
-	console.log('q');
-}
+
 
 canvas.addEventListener("mousedown", function(event) {
 	cursorx = event.offsetX;
 	cursory = event.offsetY;
  	if(cursorx > x+230 && cursorx < x+280 && cursory > y && cursory < y+80) {
- 		point += rnd+1;
+ 		point += rnd + 1;
  	}
 });
+
+backgroundImg.onload = function () {
+	game();
+}
 
 function getRandomInt(max) {
 	return Math.floor(Math.random() * max);
@@ -64,9 +51,16 @@ function game() {
 		render();
 		requestAnimationFrame(game);
 	}
-	if(result == true) {
+
+	if(result) {
 		result = false;
-		game();
+		y = 200;
+		x = getRandomInt(200);
+		rnd = getRandomInt(9);
+		step = 0;
+		point = 0;
+		age = 0;
+		setTimeout(game, 1000);
 	}
 }
 
@@ -81,19 +75,19 @@ function render() {
 
 function update() {
 	y += startSpeed - step;
-	if(y > 700) {
+	if (y > 700) {
 		y = 200;
 		x = getRandomInt(200);
 		rnd = getRandomInt(9);
-		age += 1;
-		if(point % 2 == 0 && age == maxAge && point != 0) {
+		age++;
+		if (point % 2 === 0 && age === maxAge && point != 0) {
 			saves.push(point);
 			updateLocal();
 			best = Math.max(...JSON.parse(localStorage.getItem('saves')));
 			result = confirm('Вы выйграли! Начать заново?');
-		} else {
-			if(age == maxAge) result = confirm('Вы прогиграли! Начать заново?');
+		} else if (age === maxAge) {
+			result = confirm('Вы проиграли! Начать заново?');
 		}
-		if(step < endSpeed) step += 1;
+		if (step < endSpeed) step++;
 	}
 }
